88eecf0b9af2241b2134c819371e9f01763306e8
Fabio Flauzino <fabio@mfrural.com.br>
2024-09-26 18:16:47 -0300

    Exemplo de criação de formulário com react-hook-form e zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 import { TeamArea } from "@/components/team/team-area";
 import { ThemeToggle } from "@/components/theme-toggle";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
 
 export default function Page() {
     return (
-        <div className="min-h-screen flex justify-center items-center">
-            <div className="w-full max-w-2xl">
-                <ThemeToggle />
-                
-            </div>
+        <div className="min-h-screen w-full max-w-2xl mx-auto">
+
+            <ThemeToggle />
+
+            <Card className="mt-5">
+                <CardHeader>
+                    <CardTitle>Faça o seu Login</CardTitle>
+                    <CardDescription>Digite seus dados de login</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    ...
+                </CardContent>
+            </Card>
+
         </div>
     )
 }
